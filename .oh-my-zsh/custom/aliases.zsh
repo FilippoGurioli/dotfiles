121b33b2b7358db90176272e74b29fef71d71751
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-07-05 14:06:47 +0200

    feat: add alias for hyperland config

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,6 +1,7 @@
 # Languages aliases
 alias prolog="java -jar ~/Documents/Unibo/PPS/Labs/2p-4.0.3.jar"
 alias scala="scala3"
+alias hyprconfig="code ~/.config/hypr"
 
 # Git aliases
 alias gs="git status"
