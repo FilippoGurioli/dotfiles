7ace0a28c9da060ecbb2a2acedf4d477406b2d2c
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-07-06 17:56:16 +0200

    feat: add hyprland, waybar and wofi configs

---
+++ .config/waybar/scripts/distro-logo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Get distro ID from /etc/os-release
+distro=$(grep '^ID=' /etc/os-release | cut -d= -f2 |  tr '[:upper:]' '[:lower:]' | tr -d '"')
+
+# Define folder where logos are stored
+logo_dir="$HOME/.config/waybar/distro-logos"
+
+# Default to generic logo if distro logo not found
+logo_file="$logo_dir/$distro.png"
+# if [[ ! -f "$logo_file" ]]; then
+    logo_file="$logo_dir/generic.png"
+# fi
+
+echo $logo_file
