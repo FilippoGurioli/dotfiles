a5b1b65e57efd83d068a7f72e658c8f87eb1ce93
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-07-11 16:59:32 +0200

    feat: create themer to generate wofi and waybar css from scss

---
+++ .config/themer/update-theme.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sassc ~/.config/themer/wofi.scss   ~/.config/wofi/style.css
+sassc ~/.config/themer/waybar.scss ~/.config/waybar/style.css
--- .config/waybar/scripts/launch.sh
+++ .config/waybar/scripts/launch.sh
@@ -2,4 +2,7 @@
 
 killall -9 waybar
 
+# recompile the stylesheets
+~/.config/themer/update-theme.sh
+
 waybar &
