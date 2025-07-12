41982a188ea52eba74fab95b470d4a032ac0b730
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-07-12 15:14:35 +0200

    feat: generate correctly conf file

--- .config/themer/parse-scss-vars.sh
+++ .config/themer/parse-scss-vars.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-echo "" > $2
+input_file="$1"
+output_sh="${2:-$1.sh}"
+output_conf="${3:-$1.conf}"
+
+# initialize only if this is the top-level call
+if [[ -z $2 ]]; then
+    echo "" > "$output_sh"
+    echo "" > "$output_conf"
+fi
+
+declare -A scss_vars
 
 while IFS= read -r line || [[ -n $line ]]; do
     # if line starts with @import, parse the file and extract variables
@@ -9,7 +19,7 @@ while IFS= read -r line || [[ -n $line ]]; do
         abs_path="$(dirname "$1")/$file"
         if [[ -f $abs_path ]]; then
             # recursively call the script to parse the imported file
-            bash ~/.config/themer/parse-scss-vars.sh $abs_path $2
+            source ~/.config/themer/parse-scss-vars.sh $abs_path "$output_sh" "$output_conf"
         else 
             echo "File $abs_path not found, skipping."
         fi
@@ -17,6 +27,11 @@ while IFS= read -r line || [[ -n $line ]]; do
     elif [[ $line == \$* ]]; then
         var_name=$(echo $line | cut -d':' -f1 | sed 's/^\$//' | sed 's/-/_/g')
         var_value=$(echo $line | cut -d':' -f2- | sed 's/;//g' | xargs)
-        echo "export $var_name=\"$var_value\"" >> $2
+        if [[ -v scss_vars["${var_value:1}"] ]]; then
+            var_value="${scss_vars["${var_value:1}"]}"
+        fi
+        scss_vars["$var_name"]="$var_value"
+        echo "export $var_name=\"$var_value\"" >> $output_sh
+        echo "\$$var_name = $var_value" >> $output_conf
     fi
 done < "$1"
--- .config/themer/update-theme.sh
+++ .config/themer/update-theme.sh
@@ -3,8 +3,8 @@
 sassc ~/.config/themer/wofi.scss   ~/.config/wofi/style.css
 sassc ~/.config/themer/waybar.scss ~/.config/waybar/style.css
 
-~/.config/themer/parse-scss-vars.sh ~/.config/themer/my-theme.scss ~/.config/themer/my-theme.sh
-source ~/.config/themer/my-theme.sh
+~/.config/themer/parse-scss-vars.sh ~/.config/themer/my-theme.scss
+source ~/.config/themer/my-theme.scss.sh
 
 ~/.config/themer/expand-env-vars.sh ~/.config/themer/kitty.conf ~/.config/kitty/kitty.conf
 ~/.config/themer/expand-env-vars.sh ~/.config/themer/waybar.json ~/.config/waybar/config
