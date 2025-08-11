43efd1738b3dfec1f2e10596e6cf0be2db06da2a
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-08-11 22:33:34 +0200

    feat: create bare minimum custom arch install

--- env-automation/install-custom-arch.sh
+++ env-automation/install-custom-arch.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail # fail fast strategy
 
+# Checking if the hostname is provided as first parameter
+if [ $# -ne 2 ]; then
+	echo "Error: provide the host name as first parameter (e.g. desktop/laptop/...) and the root password as second"
+	return 1
+fi
+
 # Setting an env var to spot if is vm
 if grep -qEi 'QEMU' /sys/class/dmi/id/sys_vendor 2>/dev/null; then
 	VM_ENV=1
@@ -12,7 +18,7 @@ echo "FIRST BOOT SCRIPT"
 
 echo "Checking connectivity"
 if ! ping -c 1 -W 2 8.8.8.8 &> /dev/null; then
-	echo "No connection aborting minimal setup, re-launch this script once there is connectivity"
+	echo "No connection, aborting minimal setup, re-launch this script once there is connectivity"
 	return 1
 else
 	echo "Connected, starting minimal setup"
@@ -22,7 +28,7 @@ echo "Updating system clock"
 timedatectl set-ntp true
 
 echo "Selecting the fastest 10 mirrors"
-reflector --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
+reflector --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
 echo "Partitioning the disk"
 if [ $VM_ENV ]; then
@@ -44,3 +50,87 @@ echo "OTHERS: $OTHER_DISKS"
 
 echo "Wiping the existing partitions in $SSD_PATH"
 sgdisk --zap-all "$SSD_PATH" 2>/dev/null
+echo "Creating EFI + ROOT partitions"
+parted -s "$SSD_PATH" mklabel gpt #GUID Partition Table
+parted -s "$SSD_PATH" mkpart EFI fat32 1MiB 513MiB #First partition, name:EFI, type:fat32, from 1MiB to 513 MiB (leaving first MiB blank)
+parted -s "$SSD_PATH" set 1 esp on #Setting first partition as EFI System Partition (esp)
+parted -s "$SSD_PATH" mkpart ROOT ext4 513MiB 100% #Second partition, name:ROOT, type:ext4, from 513MiB to 100% (end of disk) - the root file system
+
+EFI_PART="${SSD_PATH}1"
+ROOT_PART="${SSD_PATH}2"
+
+echo "Formatting partitions"
+mkfs.fat -F32 "$EFI_PART"
+mkfs.ext4 -F "$ROOT_PART"
+
+echo "Mounting EFI + ROOT"
+mount "$ROOT_PART" /mnt 2>/dev/null
+mkdir /mnt/boot
+mount "$EFI_PART" /mnt/boot 2>/dev/null
+
+echo "Installing base functionalities"
+pacstrap -K /mnt base linux-zen linux-firmware grub efibootmgr vim
+
+echo "Generating file systems table"
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "Mounting other disks"
+for disk in "${OTHER_DISKS[@]}"; do
+	DISK_PATH="/dev/$disk"
+	PARTITIONS=$(lsblk -nrpo NAME "$DISK_PATH" | grep -v "$DISK_PATH")
+	if [ -z "$PARTITIONS" ]; then
+		echo "No partitions on $disk - skipping"
+		continue
+	fi
+
+	for part in $PARTITIONS; do
+		PART_NAME=$(basename "$part")
+		MOUNT_DIR="/mnt/$PART_NAME"
+		mkdir -p "$MOUNT_DIR"
+		mount "$part" "$MOUNT_DIR"
+		echo "Mounted $part"
+	done
+done
+
+echo "Changing root to /mnt"
+arch-chroot /mnt
+
+echo "Setting time zone"
+ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
+
+echo "Setting localization"
+echo "en_GB.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "it_IT.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "LC_ADDRESS=it_IT.UTF-8" >> /etc/locale.conf
+echo "LC_IDENTIFICATION=it_IT.UTF-8" >> /etc/locale.conf
+echo "LC_MEASUREMENT=it_IT.UTF-8" >> /etc/locale.conf
+echo "LC_MONETARY=it_IT.UTF-8" >> /etc/locale.conf
+echo "LC_NAME=it_IT.UTF-8" >> /etc/locale.conf
+echo "LC_NUMERIC=it_IT.UTF-8" >> /etc/locale.conf
+echo "LC_PAPER=it_IT.UTF-8" >> /etc/locale.conf
+echo "LC_TELEPHONE=it_IT.UTF-8" >> /etc/locale.conf
+echo "LC_TIME=it_IT.UTF-8" >> /etc/locale.conf
+
+echo "Setting hostname to arch-$1"
+echo "arch-$1" > /etc/hostname
+
+echo "Setting hosts"
+echo "127.0.0.1 localhost" > /etc/hosts
+echo "::1 localhost" >> /etc/hosts
+echo "127.0.1.1 $1.localdomain $1" >> /etc/hosts
+
+echo "Setting the root password to $2"
+echo "root:$2" | chpasswd
+
+echo "Installing bootloader"
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "MINIMAL INSTALLATION DONE"
+echo "Rebooting"
+umount -R /mnt
+reboot
+exit 0
