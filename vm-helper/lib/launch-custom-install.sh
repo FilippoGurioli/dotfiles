11c91f9f39004eb5af44ccd72e2d7f3c2da3a75e
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-08-18 18:56:26 +0200

    correct launch custom install

--- vm-helper/lib/launch-custom-install.sh
+++ vm-helper/lib/launch-custom-install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./arch-exec.sh "curl -fsSL https://raw.githubusercontent.com/FilippoGurioli/dotfiles/master/env-automation/install.sh | bash -s -- $1 $2"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+"$SCRIPT_DIR/arch-exec.sh" "curl -fsSL https://raw.githubusercontent.com/FilippoGurioli/dotfiles/master/env-automation/install.sh | bash -s -- $1 $2"
