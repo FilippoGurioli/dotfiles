d1c9f09dad722a1be6cc490dfe8f33bfaf6222b3
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2026-02-04 17:46:51 +0100

    feat: add nvim configuration

---
+++ .config/nvim/lua/plugins/color-scheme.lua
@@ -0,0 +1,11 @@
+return {
+  "christianchiarulli/nvcode-color-schemes.vim",
+  "bluz71/vim-moonfly-colors",
+  "blazkowolf/gruber-darker.nvim",
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  opts = {
+    filters = {
+      dotfiles = false,
+    },
+    update_cwd = true,
+    respect_buf_cwd = true,
+  },
+  config = true,
+}
